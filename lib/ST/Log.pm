013c54bc4253912d6f5089ac4c6767e1c330023f
Torsten Brumm <technik@picturepunxx.de>
2017-09-26 09:51:55 +0200

    Logging

--- lib/ST/Log.pm
+++ lib/ST/Log.pm
@@ -1,4 +1,8 @@
 #!/usr/bin/env perl
+# Logging kann massiv I/O's erzeugen, daher nur nutzen, wenn nicht direkt auf
+# den Raspberry PI (SD Karte) geschrieben wird. Ich werde im Setup noch einen
+# USB Stick etc. verwenden, wo auch die Test Fotos drauf landen für die
+# Luminanzbestimmung
 
 use strict;
 use warnings;
