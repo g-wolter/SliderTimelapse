6efa248f3100b1dfa10fff6746b242c0d0f0a64c
Torsten Brumm <technik@picturepunxx.de>
2017-09-26 14:40:49 +0200

    more logging

--- bin/SliderTimelapse.pl
+++ bin/SliderTimelapse.pl
@@ -14,9 +14,13 @@ use ST::Slide;
 use ST::Analyze;
 use ST::Log;
 use POSIX qw/ strftime /;
-use Data::Dumper;
 
 my $date = strftime("%Y%m%d%H%M%S", localtime);
+my $file = "/opt/SliderTimelapse/pic/Sonnenuntergang.jpg";
+my $session = strftime("%Y-%m-%d", localtime); #Wird später durch den Sequenznamen ersetzt
+
+my $run = "\[$session\]-\[$date\] ===========================================";
+my $log = ST::Log->log($run);
 
 my $lum = ST::Luminanz->GetLuminanceInfo($file);
 print "Luminanz: $lum\n";
@@ -25,5 +29,5 @@ my ($iso, $shutter) = ST::Analyze->GetImageInfo($file);
 print "Hauptprogramm - ISO: $iso \n";
 print "Hauptprogramm - SHU: $shutter \n";
 
-my $logentry = "\[$date\] \[ISO: $iso\] \[SHU: $shutter\] \[LUM: $lum\]";
-my $log = ST::Log->log($logentry);
+my $logentry = "\[$session\] \[$date\] \[ISO: $iso\] \[SHU: $shutter\] \[LUM: $lum\]";
+$log = ST::Log->log($logentry);
--- lib/ST/Log.pm
+++ lib/ST/Log.pm
@@ -3,6 +3,9 @@
 # den Raspberry PI (SD Karte) geschrieben wird. Ich werde im Setup noch einen
 # USB Stick etc. verwenden, wo auch die Test Fotos drauf landen für die
 # Luminanzbestimmung
+# Logformat: [UniqueID] [Timestamp] [ISO: Wert] [SHU: Wert] [LUM: Wert]....
+# UniqueID stellt hierbei die Sequenz des Timelapse dar, also den Namen,
+# den der User auswählen muss und ist für die komplette Sequenz vorhanden.
 
 use strict;
 use warnings;
