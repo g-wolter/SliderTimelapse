3211fbbd7f4fda74988dab45c074c31d3896c8ec
Torsten Brumm <technik@picturepunxx.de>
2017-09-26 16:54:22 +0200

    Floating Average Calc

--- bin/SliderTimelapse-Test.pl
+++ bin/SliderTimelapse-Test.pl
@@ -19,11 +19,14 @@ use POSIX qw/ strftime /;
 use File::Type;
 
 my $date = strftime("%Y%m%d%H%M%S", localtime);
-my $session = strftime("%Y-%m-%d", localtime); #Wird später durch den Sequenznamen ersetzt
-my $run = "\[$session\]-\[$date\] ===========================================";
+my $session = "Sonnenuntergang";
+#strftime("%Y-%m-%d", localtime); #Wird später durch den Sequenznamen ersetzt
+my @lum;
+
+my $run = "\[$session\] \[$date\] ===========================================";
 my $log = ST::Log->log($run);
 
-my $data_dir = "/opt/SliderTimelapse/pic/";
+my $data_dir = ".";
 opendir( DATA_DIR, $data_dir ) || die "Cannot open $data_dir\n";
 my @files = readdir(DATA_DIR);
 @files = sort @files;
@@ -35,15 +38,22 @@ foreach my $file (@files) {
 	#If it's an image file, add it to the luminance hash.
 	if ( $filetype eq "image" ) {
 
-
     my $lum = ST::Luminanz->GetLuminanceInfo($file);
     print "Luminanz: $lum\n";
 
     my ($iso, $shutter) = ST::Analyze->GetImageInfo($file);
+    print "File: $file\n";
     print "Hauptprogramm - ISO: $iso \n";
     print "Hauptprogramm - SHU: $shutter \n";
 
-    my $logentry = "\[$session\] \[$date\] \[ISO: $iso\] \[SHU: $shutter\] \[LUM: $lum\]";
+    push(@lum, $lum);
+    print "Array: ";
+    print @lum;
+    print "\n";
+
+    my $float = ST::Float->GetFloatingLuminance(\@lum);
+
+    my $logentry = "\[$session\] \[$date\] \[ISO: $iso\] \[SHU: $shutter\] \[LUM: $lum\] \[File: $file\]";
     $log = ST::Log->log($logentry);
 
 	}
--- lib/ST/Float.pm
+++ lib/ST/Float.pm
@@ -10,6 +10,22 @@ sub new {
   return;
 }
 
+sub GetFloatingLuminance {
+  my $self = shift;
+
+  my ($Referenz_auf_ARRAY) = @_;
+  my @lum = @{$Referenz_auf_ARRAY};
+
+  if(scalar(@lum) > 0){
+    my $sum = 0;
+    $sum += $_ for(@lum);
+    my $avg = $sum / scalar(@lum);
+    print "Floating Luminance: ";
+    print $avg,"\n";
+  }
+
+}
+
 sub test {
   print "Hallo aus der Routine Float";
   return;
