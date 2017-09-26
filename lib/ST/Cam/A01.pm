cc06b3d7e85e83011bb35b1719512af1a257dfdb
Torsten Brumm <technik@picturepunxx.de>
2017-09-26 08:33:59 +0200

    Start with A01

---
+++ lib/ST/Cam/A01.pm
@@ -0,0 +1,16 @@
+#!/usr/bin/env perl
+# Analyze every taken picture and store the lens, shutter and iso information
+# for later usage.
+
+use strict;
+use warnings;
+use feature 'say';
+package ST::Cam::A01;
+use Data::Dumper;
+use Getopt::Std;
+
+sub new {
+  return;
+}
+
+1;
